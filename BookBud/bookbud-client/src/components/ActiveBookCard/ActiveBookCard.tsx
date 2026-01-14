eee193bee19d3177fce5addff35861461870b52c
Karin Henriksson <karin.henriksson@medieinstitutet.se>
2026-01-14 14:43:51 +0100

    ✨ feat: replace moch data with real data for progress

--- BookBud/bookbud-client/src/components/ActiveBookCard/ActiveBookCard.tsx
+++ BookBud/bookbud-client/src/components/ActiveBookCard/ActiveBookCard.tsx
@@ -3,18 +3,37 @@ import { useEffect, useState } from 'react';
 import { Book } from '../../models/book';
 import { ProgressBar } from '../ProgressBar/ProgressBar';
 import { booksService } from '../../services/booksService';
+import { progressService } from '../../services/progressService';
+import { authService } from '../../services/authService';
+import { calcProgressPercent } from '../../helpers/calcProgressPercent';
 
 export const ActiveBookCard = () => {
 
   const [activeBook, setActiveBook] = useState<Book | null>(null);
+  const [pagesRead, setPagesRead] = useState(0)
   const [isLoading, setIsLoading] = useState(true);
 
   
   useEffect(() => {
-    booksService.getActiveBook()
-    .then(setActiveBook)
-    .catch(err => console.error('Error fetching active book:', err))
-    .finally(() => setIsLoading(false));
+    const fetchData = async () => {
+      try {
+        const activeBook = await booksService.getActiveBook();
+        setActiveBook(activeBook);
+  
+        const progressData = await progressService.getProgressForActiveBook();
+  
+        //find the logged in users progress for the active book
+        const user = authService.getCurrentUser();
+        const userId = user?.id;
+        const usersProgress = progressData.members.find((member) => member.userId === userId);
+        setPagesRead(usersProgress?.pagesRead ?? 0);
+      } catch (error) {
+      console.error('Error fetching active book or progress:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
   }, []);
 
   if (isLoading) {
@@ -27,8 +46,7 @@ export const ActiveBookCard = () => {
 
 
   const totalPages = activeBook.pageCount;
-  const pagesRead = 75; //mock for now
-  const progressPercent = Math.floor((pagesRead / totalPages) * 100);
+  const progressPercent = calcProgressPercent(pagesRead, totalPages)
 
   return (
     <div className="book-card">
