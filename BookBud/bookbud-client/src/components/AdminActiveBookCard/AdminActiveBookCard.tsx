7fc2330c9f1fc3eb33111c7a32cebb0cbaefee76
Karin Henriksson <karin.henriksson@medieinstitutet.se>
2026-01-19 10:55:35 +0100

    ✨ feat: create AdminActiveBookCard

---
+++ BookBud/bookbud-client/src/components/AdminActiveBookCard/AdminActiveBookCard.tsx
@@ -0,0 +1,29 @@
+import './AdminActiveBookCard.scss';
+import type { Book } from '../../models/book';
+
+interface AdminActiveBookCardProps {
+    book: Book
+    onMarkAsFinished: (book: Book) => void
+}
+
+export const AdminActiveBookCard = ( { book, onMarkAsFinished }: AdminActiveBookCardProps) => {
+    return (
+        <div className="admin-active-book-card">
+            <img src={book.coverUrl} alt={book.title} />
+            <div className='book-info'>
+                <h3>{book.title}</h3>
+                <p>by {book.author}</p>
+                <p>{book.pageCount} pages</p>
+                <p>Published: {book.year}</p>
+                {onMarkAsFinished && (
+                    <button 
+                    className="mark-finished-btn"
+                    onClick={() => onMarkAsFinished(book)}
+                    >
+                    Mark as Finished
+                    </button>
+                )}
+            </div>
+        </div>
+    )
+}
